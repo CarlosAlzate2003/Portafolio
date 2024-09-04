6321a0945dc58eaff800d338c4b6e111ce6c62c2
Carlos Andres Alzate Mejia <carlosalzate1601@gmail.com>
2024-09-03 23:44:30 -0500

    add responsive v2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav>
       <ul className="Navbar-ul">
         <li className="Navbar-li">
-          <Link to="/">Sobre mí</Link>
+          <Link to="/">Experiencia</Link>
         </li>
         <li className="Navbar-li">
           <Link to="/proyectos">Proyectos</Link>
