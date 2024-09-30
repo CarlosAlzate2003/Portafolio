d5481867d1b0dbe8f9d84b79127585b34c21e5a1
CarlosAlzate2003 <carlosalzate1601@gmail.com>
2024-09-30 17:50:29 -0500

    Changes tools

--- src/pages/Tools.jsx
+++ src/pages/Tools.jsx
@@ -7,25 +7,25 @@ const Tools = () => {
       <div className="flex-tools">
         <div className="flex-item">
           <a href="https://www.python.org" target="_blank">
-            <img src="/assets\svg\python.svg" alt="python_icon" />
+            <img src="public\assets\svg\python.svg" alt="python_icon" />
           </a>
           <p>Python</p>
         </div>
         <div className="flex-item">
           <a href="https://react.dev/" target="_blank">
-            <img src="/assets\svg\react.svg" alt="react_icon" />
+            <img src="public\assets\svg\react.svg" alt="react_icon" />
           </a>
           <p>React</p>
         </div>
         <div className="flex-item">
           <a href="https://www.postman.com/downloads/" target="_blank">
-            <img src="/assets\svg\postman.svg" alt="postman_icon" />
+            <img src="public\assets\svg\postman.svg" alt="postman_icon" />
           </a>
           <p>Postman</p>
         </div>
         <div className="flex-item">
           <a href="https://www.java.com/es/" target="_blank">
-            <img src="/assets\svg\java.svg" alt="java_icon" />
+            <img src="public\assets\svg\java.svg" alt="java_icon" />
           </a>
           <p>Java</p>
         </div>
@@ -34,13 +34,13 @@ const Tools = () => {
             href="https://www.php.net/manual/es/intro-whatis.php"
             target="_blank"
           >
-            <img src="/assets\svg\php.svg" alt="php_icon" />
+            <img src="public\assets\svg\php.svg" alt="php_icon" />
           </a>
           <p>php</p>
         </div>
         <div className="flex-item">
           <a href="https://www.postgresql.org/" target="_blank">
-            <img src="/assets\svg\postgres.svg" alt="postgres_icon" />
+            <img src="public\assets\svg\postgres.svg" alt="postgres_icon" />
           </a>
           <p>PostgreSQL</p>
         </div>
@@ -49,19 +49,19 @@ const Tools = () => {
             href="https://www.microsoft.com/es-co/sql-server/sql-server-2022"
             target="_blank"
           >
-            <img src="/assets\svg\sqlserver.svg" alt="sqlserver_icon" />
+            <img src="public\assets\svg\sqlserver.svg" alt="sqlserver_icon" />
           </a>
           <p>SQL Server</p>
         </div>
         <div className="flex-item">
           <a href="https://www.mysql.com/" target="_blank">
-            <img src="/assets\svg\mysql.svg" alt="mysql_icon" />
+            <img src="public\assets\svg\mysql.svg" alt="mysql_icon" />
           </a>
           <p>MySQL</p>
         </div>
         <div className="flex-item">
           <a href="https://git-scm.com/" target="_blank">
-            <img src="/assets\svg\git.svg" alt="git_icon" />
+            <img src="public\assets\svg\git.svg" alt="git_icon" />
           </a>
           <p>Git</p>
         </div>
@@ -70,7 +70,7 @@ const Tools = () => {
             href="https://dotnet.microsoft.com/es-es/languages/csharp"
             target="_blank"
           >
-            <img src="/assets\svg\csharp.svg" alt="cshare_icon" />
+            <img src="public\assets\svg\csharp.svg" alt="cshare_icon" />
           </a>
           <p>C#</p>
         </div>
@@ -79,7 +79,7 @@ const Tools = () => {
             href="https://developer.android.com/studio?gad_source=1&gclid=CjwKCAjw59q2BhBOEiwAKc0ijZRyt3e7w0LR9ETG44Q_IpsuUsWeB5RT2WMn5NXZ6_lAHkWUbkMItBoCRRAQAvD_BwE&gclsrc=aw.ds&hl=es-419"
             target="_blank"
           >
-            <img src="/assets\svg\android.svg" alt="android_icon" />
+            <img src="public\assets\svg\android.svg" alt="android_icon" />
           </a>
           <p>Android Studio</p>
         </div>
@@ -88,7 +88,7 @@ const Tools = () => {
             href="https://www.microsoft.com/es-co/microsoft-365/microsoft-office"
             target="_blank"
           >
-            <img src="/assets\svg\microsoft.svg" alt="microsoft_icon" />
+            <img src="public\assets\svg\microsoft.svg" alt="microsoft_icon" />
           </a>
           <p>Herramientas Microsoft Office</p>
         </div>
