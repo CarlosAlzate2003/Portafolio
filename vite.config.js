a535af78be8b5437bbebeadaa83519d8bb1f8374
CarlosAlzate2003 <carlosalzate1601@gmail.com>
2024-09-30 17:28:55 -0500

    fI

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'https://github.com/CarlosAlzate2003/Portafolio',
   plugins: [react()],
 })
