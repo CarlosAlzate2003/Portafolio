9cf6615d9d1db46879dbd2f7f0174cd963bf2eb2
Carlos Andres Alzate Mejia <carlosalzate1601@gmail.com>
2024-10-06 22:01:35 -0500

    add certficate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,12 +22,30 @@ const Home = () => {
           <h2>Formación académica</h2>
           <li>
             <p>Tecnólogo en Desarrollo de Software en el Pascual Bravo</p>
+            <a
+              href="https://drive.google.com/file/d/1SxR_zttwsJiL31uZz4guAgUOeOMW3WI2/view"
+              target="_blank"
+            >
+              Ver certificado
+            </a>
           </li>
           <li>
             <p>Técnico de Desarrollo Fullstack lvl 2</p>
+            <a
+              href="https://drive.google.com/file/d/1PAux7kYlE9NQBAaFoVUpCoEH9hPQ11TW/view"
+              target="_blank"
+            >
+              Ver certificado
+            </a>
           </li>
           <li>
             <p>Diplomas en cursos de base de datos, React, Python y Java</p>
+            <a
+              href="https://drive.google.com/drive/folders/1-gSauMKmtQm4KqV2xRX1SopOZHmbEE_3"
+              target="_blank"
+            >
+              Ver certificado
+            </a>
           </li>
         </ul>
       </ul>
